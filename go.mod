0842d0dc937ae0e74cf0ace9d485336aaaa40477
Jens Hilligsøe <jens@hilli.dk>
2021-10-28 13:08:51 +0200

    Make snapshot from commandline parsed URL

--- go.mod
+++ go.mod
@@ -10,6 +10,8 @@ require (
 	github.com/gobwas/pool v0.2.1 // indirect
 	github.com/gobwas/ws v1.1.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
+	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
+	golang.org/x/net v0.0.0-20211020060615-d418f374d309 // indirect
 	golang.org/x/sys v0.0.0-20211025201205-69cdffdb9359 // indirect
 )
--- web-screenshot.go
+++ web-screenshot.go
@@ -4,46 +4,51 @@ import (
 	"context"
 	"io/ioutil"
 	"log"
+	"net/url"
+	"os"
+	"strings"
+	"time"
 
+	"github.com/chromedp/cdproto/runtime"
 	"github.com/chromedp/chromedp"
+	"github.com/kennygrant/sanitize"
 )
 
 func main() {
-	// create context
+	if len(os.Args) < 2 {
+		log.Fatal("usage: web-screenshot <url>")
+	}
+	myURL := os.Args[1]
+	if myURL == "" {
+		log.Fatal("No URL provided")
+	} else {
+		log.Printf("URL: %s", myURL)
+	}
+	// create contexts
 	ctx, cancel := chromedp.NewContext(
 		context.Background(),
-		chromedp.WithDebugf(log.Printf),
+		// chromedp.WithDebugf(log.Printf),
 	)
+	ctx2, cancel2 := context.WithTimeout(ctx, time.Second*10)
 	defer cancel()
+	defer cancel2()
 
-	// capture screenshot of an element
 	var buf []byte
-	if err := chromedp.Run(ctx, elementScreenshot(`https://pkg.go.dev/`, `img.Homepage-logo`, &buf)); err != nil {
-		log.Fatal(err)
-	}
-	if err := ioutil.WriteFile("elementScreenshot.png", buf, 0o644); err != nil {
-		log.Fatal(err)
-	}
 
 	// capture entire browser viewport, returning png with quality=90
-	if err := chromedp.Run(ctx, fullScreenshot(`https://dr.dk/`, 90, &buf)); err != nil {
+	if err := chromedp.Run(ctx2, fullScreenshot(myURL, 90, &buf)); err != nil {
 		log.Fatal(err)
 	}
-	if err := ioutil.WriteFile("fullScreenshot.png", buf, 0o644); err != nil {
+	u, err := url.Parse(myURL)
+	if err != nil {
 		log.Fatal(err)
 	}
-
-	log.Printf("wrote elementScreenshot.png and fullScreenshot.png")
-}
-
-// elementScreenshot takes a screenshot of a specific element.
-func elementScreenshot(urlstr, sel string, res *[]byte) chromedp.Tasks {
-	return chromedp.Tasks{
-		chromedp.Navigate(urlstr),
-		// chromedp.SetContent(sel, `{ waitUntil: "load" }`),
-		chromedp.Poll("document.fonts.ready", res)
-		chromedp.Screenshot(sel, res, chromedp.NodeVisible),
+	imageFile := sanitize.Path("screenshot-" + u.Host + u.Path + ".png")
+	imageFile = strings.Replace(imageFile, "/", "-", -1)
+	if err := ioutil.WriteFile(imageFile, buf, 0o644); err != nil {
+		log.Fatal(err)
 	}
+	log.Printf("wrote %s", imageFile)
 }
 
 // fullScreenshot takes a screenshot of the entire browser viewport.
@@ -52,6 +57,17 @@ func elementScreenshot(urlstr, sel string, res *[]byte) chromedp.Tasks {
 func fullScreenshot(urlstr string, quality int, res *[]byte) chromedp.Tasks {
 	return chromedp.Tasks{
 		chromedp.Navigate(urlstr),
+		chromedp.Poll("document.fonts.ready", res),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			_, exp, err := runtime.Evaluate(`window.scrollTo(0,document.body.scrollHeight);`).Do(ctx)
+			if err != nil {
+				return err
+			}
+			if exp != nil {
+				return exp
+			}
+			return nil
+		}),
 		chromedp.FullScreenshot(res, quality),
 	}
 }
